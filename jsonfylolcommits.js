c272f2b4aed0da52bbfb33388c62ad9ba4fbfc66
Lucas Vieira <vieiralucas4@gmail.com>
2014-10-14 01:27:52 -0300

    add feedback, add commander to output help, version and offer quiet option

--- jsonfylolcommits.js
+++ jsonfylolcommits.js
@@ -2,18 +2,50 @@
 
 var fs             = require('fs'),
     path           = require('path'),
+    colors         = require('colors'),
+    commander      = require('commander'),
     homedir        = (process.platform === 'win32') ? process.env.HOMEPATH : process.env.HOME,
     lolcommitsdir  = homedir + '/.lolcommits',
-    jsonData       = [],
-    outputFilePath = process.argv[2];
+    jsonData       = [];
 
+var finished = 0, total = 0;
+
+commander
+  .version('1.0.0')
+  .option('-q, --quiet', 'run without feedback')
+
+commander.on('--help', function(){
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ jsonfylolcommits out.json');
+  console.log('    $ jsonfylolcommits out.json -q');
+  console.log('');
+});
+
+commander.parse(process.argv);
+
+var outputFilePath = commander.args[0];
+if(typeof(outputFilePath) == 'undefined') {
+    commander.help();
+}
 
 loopThroughFiles(lolcommitsdir);
 
+
 // recursive walk through file tree
 function loopThroughFiles(dir) {
-
     var files = fs.readdirSync(dir);
+    if(dir !== lolcommitsdir) {
+        if(!commander.quite) {
+            console.log('\n' + path.basename(dir).yellow);
+        }
+    } else {
+        for(var i = 0; i < files.length; i++) {
+            if(fs.lstatSync(dir + '/' + files[i]).isDirectory()) {
+                total++;
+            }
+        }
+    }
     for (var i = 0; i < files.length; i++) {
         var file = files[i];
         var filePath = dir + '/' + file;
@@ -32,9 +64,17 @@ function base64(filePath) {
     var file = fs.readFileSync(filePath),
         json = {};
     json[path.basename(filePath).replace('.jpg', '')] = file.toString('base64');
+    if(!commander.quite) {
+        console.log(' |_ ' + path.basename(filePath).green + ' converted to base64'.green);
+    }
     jsonData.push(json);
 }
 
 function writeFile() {
     fs.writeFileSync(outputFilePath, JSON.stringify(jsonData, null, 4));
+    finished++;
+    if(finished === total) {
+        var newLine = commander.quite ? '' : '\n';
+        console.log(newLine + 'Output file saved to ' + outputFilePath + '\n');
+    }
 }
